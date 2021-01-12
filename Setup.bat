b129dc3adfd3acaa4026d80defe32f894c3f455c
Alyssa <zzinggame@gmail.com>
2021-01-13 01:43:25 +0700

    Initial commit

--- Setup.bat
+++ Setup.bat
@@ -6,7 +6,7 @@ echo .
 powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 choco feature enable -n=allowGlobalConfirmation
 cls
-choco install python
+choco install python -y
 cls
 call refreshenv
 cls
