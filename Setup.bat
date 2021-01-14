b5846fd6b6fbdaadcac2504857a95365b6dd3db9
Alyssa <zzinggame@gmail.com>
2021-01-14 14:11:09 +0700

    Initial commit

--- Setup.bat
+++ Setup.bat
@@ -3,6 +3,28 @@ echo Alyssa Software Deployment
 echo .
 echo Ensure your running cmd in Administrator
 echo .
+NET SESSION >nul 2>&1
+IF %ERRORLEVEL% EQU 0 (
+    ECHO Administrator PRIVILEGES Detected! 
+) ELSE (
+   echo ######## ########  ########   #######  ########  
+   echo ##       ##     ## ##     ## ##     ## ##     ## 
+   echo ##       ##     ## ##     ## ##     ## ##     ## 
+   echo ######   ########  ########  ##     ## ########  
+   echo ##       ##   ##   ##   ##   ##     ## ##   ##   
+   echo ##       ##    ##  ##    ##  ##     ## ##    ##  
+   echo ######## ##     ## ##     ##  #######  ##     ## 
+   echo.
+   echo.
+   echo ####### ERROR: ADMINISTRATOR PRIVILEGES REQUIRED #########
+   echo This script must be run as administrator to work properly!  
+   echo If you're seeing this after clicking on a start menu icon, then right click on the shortcut and select "Run As Administrator".
+   echo ##########################################################
+   echo.
+   PAUSE
+   EXIT /B 1
+)
+Pushd "%~dp0"
 powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin
 choco feature enable -n=allowGlobalConfirmation
 cls
@@ -10,9 +32,11 @@ choco install python -y
 cls
 call refreshenv
 cls
-pip install -r requirements.txt --user
+pip install -r requirements.txt
 cls
 call refreshenv
 cls
 python Install.py
 call refreshenv
+cls
+python Install.py
